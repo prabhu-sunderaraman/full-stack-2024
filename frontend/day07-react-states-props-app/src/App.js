69137daae5a98e2d38fb951f5aec954354bfcf52
Prabhu Sunderaraman <prabhu.bits@gmail.com>
2024-07-04 13:08:59 +0530

    Day07

--- frontend/day07-react-states-props-app/src/App.js
+++ frontend/day07-react-states-props-app/src/App.js
@@ -3,10 +3,17 @@ import GuessingGame from './lab01/GuessingGame';
 import StateExample1 from './StateExample1';
 import StateExample2 from './StateExample2';
 import StateExample3 from './StateExample3';
+import TGIF from './TGIF';
 
 function App() {
   return (
     <div className="App">
+      <TGIF name="Vinoth" message="Movie time"/>
+      <TGIF name="Lakshmi" message="Time for Pizza"/>
+      <TGIF name="Umesh" message="Sleep well"/>
+      <TGIF name="Princy" message="Malls"/>
+      <TGIF name="Balakumar" message="Biriyani"/>
+      <hr/>
       <GuessingGame/>
     </div>
   );
---
+++ frontend/day07-react-states-props-app/src/TGIF.js
@@ -0,0 +1,9 @@
+const TGIF = ({name, message}) => {
+    return (<div>
+        <h1>Hi {name}</h1>
+        <h1>Thank god, it's Friday tomorrow!</h1>
+        <p>{message}</p>
+    </div>);
+}
+
+export default TGIF;
