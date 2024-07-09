84a496eabbb198b5cd2610d77211ba7ea727264e
Prabhu Sunderaraman <prabhu.bits@gmail.com>
2024-07-09 12:46:49 +0530

    Day10

--- frontend/day10-more-hooks-app/src/routerExample1/Menu.js
+++ frontend/day10-more-hooks-app/src/routerExample1/Menu.js
@@ -1,8 +1,11 @@
 export const Menu = () => {
-    return (<ul style={{listStyleType: 'none','margin': 0, padding: 0}}>
-        <li><a href="/">Home</a></li>
-        <li><a href="/game">Game</a></li>
-        <li><a href="/reducer">Reducer</a></li>
-        <li><a href="/contact">Contact</a></li>
-      </ul>);
+    return (<div>
+        <ul style={{listStyleType: 'none','margin': 20, padding: 0}}>
+            <li><a href="/">Home</a></li>
+            <li><a href="/game">Game</a></li>
+            <li><a href="/reducer">Reducer</a></li>
+            <li><a href="/contact">Contact</a></li>
+        </ul>
+        <hr/>
+    </div>);
 };
