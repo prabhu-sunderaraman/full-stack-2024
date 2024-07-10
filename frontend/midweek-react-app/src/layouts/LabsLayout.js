1271dffe80502c12b4397edf9df333e569cd3631
Prabhu Sunderaraman <prabhu.bits@gmail.com>
2024-07-10 12:10:26 +0530

    Day11

--- frontend/midweek-react-app/src/layouts/LabsLayout.js
+++ frontend/midweek-react-app/src/layouts/LabsLayout.js
@@ -1,12 +1,12 @@
-import { Outlet } from "react-router-dom";
+import { NavLink, Outlet } from "react-router-dom";
 
 const LabsLayout = () => {
     return (<div>
         <header>
             <h5>Here' are the labs</h5>
             <ul className='layoutList'>
-                <li><a href="/labs">Lab01</a></li>
-                <li><a href="/labs/lab02">Lab02</a></li>
+                <li><NavLink to="/labs">Lab01</NavLink></li>
+                <li><NavLink to="/labs/lab02">Lab02</NavLink></li>
             </ul>
         </header>    
         <hr/>
--- frontend/midweek-react-app/src/layouts/MainLayout.js
+++ frontend/midweek-react-app/src/layouts/MainLayout.js
@@ -1,19 +1,19 @@
-import { Outlet } from "react-router-dom";
+import { NavLink, Outlet } from "react-router-dom";
 
 const MainLayout = () => {
     return (<div>
         <header>
             <h1>Midweek react app</h1>
             <ul className='layoutList'>
-                <li><a href="/">Home</a></li>
-                <li><a href="/labs">Labs</a></li>
-                <li><a href="/contact">Contact us</a></li>
+                <li><NavLink to='/'>Home</NavLink></li>
+                <li><NavLink to='/labs'>Labs</NavLink></li>
+                <li><NavLink to='/contact'>Contact us</NavLink></li>
             </ul>
         </header>    
         <hr/>
         <main>
             <Outlet/>
-        </main>
+        </main> 
         <hr/>
         <footer>
             <p>Copyrights reserved, 2024</p>
