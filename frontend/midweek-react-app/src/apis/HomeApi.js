1978246cf8e4b3bf2bddaa7c7dc1934bef7726ea
Prabhu Sunderaraman <prabhu.bits@gmail.com>
2024-07-11 11:25:40 +0530

    Day12

--- frontend/midweek-react-app/src/apis/HomeApi.js
+++ frontend/midweek-react-app/src/apis/HomeApi.js
@@ -1,11 +1,9 @@
 const env = process.env;
 
 const loadMessage = async () => {
-    let output = '';
-    output = await fetch(`${env.REACT_APP_BACKEND_URL}/message`)
+    return await fetch(`${env.REACT_APP_BACKEND_URL}/message`)
         .then(response => response.text())
         .catch(error => error);
-    return output;    
 };
 
 
