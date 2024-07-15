a0fb05d8f119b22f706518b9a2c239a0e777ae12
Prabhu Sunderaraman <prabhu.bits@gmail.com>
2024-07-15 11:02:49 +0530

    Day14

---
+++ frontend/graphql-api/hello-server.js
@@ -0,0 +1,28 @@
+const { ApolloServer } = require("@apollo/server");
+const { startStandaloneServer } = require('@apollo/server/standalone');
+
+let schema = `
+    type Query {
+        temperature: Float
+    }
+`;
+
+let resolvers = {
+    Query: {
+        temperature() {
+            return Math.random() * 100;
+        }
+    }
+};
+
+let server = new ApolloServer({
+    typeDefs: schema,
+    resolvers
+});
+
+startStandaloneServer(server, {
+    listen: 9000
+})
+.then(response => console.log("GraphQL server started at 9000"));
+
+
