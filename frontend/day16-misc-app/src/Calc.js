98b0021ec7f7d51e98da4efbfc2e4ad9adc11296
Prabhu Sunderaraman <prabhu.bits@gmail.com>
2024-07-17 10:48:14 +0530

    Day16

--- frontend/day16-misc-app/src/Calc.js
+++ frontend/day16-misc-app/src/Calc.js
@@ -1,5 +1,5 @@
 import {useState} from 'react';
-import { addAction } from './Store';
+import { addAction, subtractAction } from './Store';
 import {useDispatch, useSelector} from 'react-redux';
 
 const Calc = () => {
@@ -7,15 +7,22 @@ const Calc = () => {
     const [number1, setNumber1] = useState();
     const [number2, setNumber2] = useState();
     const {num1, num2, sum} = useSelector(state => state.addition);
+    const {subtraction} = useSelector(state => state);
 
     const addButtonClicked = () => {
+        // Go to a backend Api and compute the sum
+        // Fetch the sum from backend and then dispatch the result to the store
+        // Another approach is use redux saga
         dispatch(addAction.add({
             num1: number1,
             num2: number2
         }));
     };
     const subtractButtonClicked = () => {
-
+        dispatch(subtractAction.subtract({
+            num1: number1,
+            num2: number2
+        }));
     };
     return (<div>
         <input type="number" placeholder="Number 1" onChange={e => setNumber1(parseInt(e.target.value))}></input> <br/>
@@ -24,7 +31,7 @@ const Calc = () => {
         <h5>Sum of {num1} and {num2} is {sum}</h5>
         <hr/>
         <button onClick={subtractButtonClicked}>Subtract</button>
-        <h5>Difference of {num1} and {num2} is </h5>
+        <h5>Difference of {subtraction.num1} and {subtraction.num2} is {subtraction.difference} </h5>
     </div>);
 };
 export default Calc;
--- frontend/day16-misc-app/src/Store.js
+++ frontend/day16-misc-app/src/Store.js
@@ -5,27 +5,49 @@ const initialState = {
         num1: 0,
         num2: 0,
         sum: 0
+    },
+    subtraction: {
+        num1: 0,
+        num2: 0,
+        difference: 0
     } 
 };
 
 const add = (addition, action) => {
+    //DO NOT WRITE SERVER SIDE CODE HERE
     let {num1, num2} = action.payload;
     let sum = num1 + num2;
     return { ...addition, num1, num2, sum};
 };
 
+const subtract = (subtraction, action) => {
+    let {num1, num2} = action.payload;
+    let difference = num1 - num2;
+    return { ...subtraction, num1, num2, difference};
+};
+
 const addSlice = createSlice({
-    name: "day16",
+    name: "day16-math",
     initialState: initialState.addition,
     reducers: {
         add
     }
 });
 
+const subtractSlice = createSlice({
+    name: "day16-calc",
+    initialState: initialState.subtraction,
+    reducers: {
+        subtract
+    }
+});
+
 export const store = configureStore({
     reducer: {
-        "addition": addSlice.reducer
+        "addition": addSlice.reducer,
+        "subtraction": subtractSlice.reducer
     }
 });
 
 export const  addAction = addSlice.actions;
+export const  subtractAction = subtractSlice.actions;
