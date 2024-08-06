3eb73e8a641fbe54f8c354fed50ffa9981835ccd
Prabhu Sunderaraman <prabhu.bits@gmail.com>
2024-08-06 13:10:43 +0530

    day11

--- backend/java_intermediate/jdbc_examples/src/main/java/com/indium/labs/lab11/Lab11.java
+++ backend/java_intermediate/jdbc_examples/src/main/java/com/indium/labs/lab11/Lab11.java
@@ -8,25 +8,25 @@ public class Lab11 {
     static DatabaseMapper databaseMapper = new DatabaseMapper();
 
     public static void main(String[] args) throws SQLException {
-        List<Object> users = loadAllUsers();
+//      Will sanitize this SQL query to avoid Injection
+        List<Object> users = loadAll("select * from users", User.class);
         users.forEach(System.out::println);
 
-        List<Object> accounts = loadAllAccounts();
+        List<Object> accounts = loadAll("select * from accounts", Account.class);
         accounts.forEach(System.out::println);
     }
 
-    private static List<Object> loadAllAccounts() throws SQLException {
+
+    private static List<Object> loadAll(String sql, Class resultClass) throws SQLException {
         Connection conn = null;
         List<Object> records = new ArrayList<>();
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
             conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/training", "root", "root");
-            String sql = "select * from accounts";
             Statement statement = conn.createStatement();
             ResultSet rs = statement.executeQuery(sql);
-            records = databaseMapper.mapResultSetToObjects(rs, Account.class);
+            records = databaseMapper.mapResultSetToObjects(rs, resultClass);
             statement.close();
-
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
@@ -35,23 +35,5 @@ public class Lab11 {
         return records;
     }
 
-    private static List<Object> loadAllUsers() throws SQLException {
-        Connection conn = null;
-        List<Object> records = new ArrayList<>();
-        try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/training", "root", "root");
-            String sql = "select * from users";
-            Statement statement = conn.createStatement();
-            ResultSet rs = statement.executeQuery(sql);
-            records = databaseMapper.mapResultSetToObjects(rs, User.class);
-            statement.close();
 
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            conn.close();
-        }
-        return records;
-    }
 }
