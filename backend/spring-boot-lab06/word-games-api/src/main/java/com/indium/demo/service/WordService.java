035bac17fd56ea1cdb78d1e1655e2f14c095aa40
Prabhu Sunderaraman <prabhu.bits@gmail.com>
2024-08-29 11:42:11 +0530

    Day 23

--- backend/spring-boot-lab06/word-games-api/src/main/java/com/indium/demo/service/WordService.java
+++ backend/spring-boot-lab06/word-games-api/src/main/java/com/indium/demo/service/WordService.java
@@ -1,5 +1,6 @@
 package com.indium.demo.service;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -8,9 +9,12 @@ import org.springframework.web.client.RestTemplate;
 public class WordService {
     private RestTemplate restTemplate = new RestTemplate();
 
+    @Value("${palindrome-api.base-url}")
+    private String palindromeApiBaseUrl;
+
     public String isPalindrome(String word) {
         // Code to connect to palindrome-api goes here
-        String palindromeApiUrl = "http://localhost:8081/word/" + word;
+        String palindromeApiUrl = palindromeApiBaseUrl + "/word/" + word;
         ResponseEntity<Boolean> result = restTemplate.getForEntity(palindromeApiUrl, Boolean.class);
         if (result.getBody()) {
             return word + " is a palindrome";
