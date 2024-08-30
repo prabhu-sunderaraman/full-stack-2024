ace271726b6134cc31e585b193a8936a4d769504
Prabhu Sunderaraman <prabhu.bits@gmail.com>
2024-08-30 14:26:16 +0530

    Day 24

--- backend/kafka-app/consumer-app/src/main/java/com/indium/consumerapp/MatchConsumer.java
+++ backend/kafka-app/consumer-app/src/main/java/com/indium/consumerapp/MatchConsumer.java
@@ -7,11 +7,48 @@ import org.springframework.stereotype.Component;
 @Component
 public class MatchConsumer {
 
-    @KafkaListener(topics = {"match-topic"}, groupId = "whatsapp-notification-group")
-    public void sendWhatsAppMessageOfMatchStatus(ConsumerRecord<String, String> matchRecord) {
-        System.out.println("**** RECEIVED ****");
+
+    @KafkaListener(topics = {"match-topic"}, groupId = "charts-group")
+    public void generateCharts1OfMatchStatus(ConsumerRecord<String, String> matchRecord) {
+        System.out.println("**** (1) CHARTS GROUP ****");
         System.out.println(matchRecord.key());
         System.out.println(matchRecord.value());
+        System.out.println(matchRecord.offset());
     }
+
+    @KafkaListener(topics = {"match-topic"}, groupId = "charts-group")
+    public void generateCharts2OfMatchStatus(ConsumerRecord<String, String> matchRecord) {
+        System.out.println("**** (2) CHARTS GROUP ****");
+        System.out.println(matchRecord.key());
+        System.out.println(matchRecord.value());
+        System.out.println(matchRecord.offset());
+    }
+
+
+    // Ideally every consumer group id is implemented as a separate spring boot consumer application
+
+//    @KafkaListener(topics = {"match-topic"}, groupId = "telegram-notification-group")
+//    public void sendTelegramOfMatchStatus(ConsumerRecord<String, String> matchRecord) {
+//        System.out.println("****Received in Telegram group ****");
+//        System.out.println(matchRecord.key());
+//        System.out.println(matchRecord.value());
+//        System.out.println(matchRecord.offset());
+//    }
+//
+//    @KafkaListener(topics = {"match-topic"}, groupId = "report-generation-group")
+//    public void reportGenerationOfMatchStatus(ConsumerRecord<String, String> matchRecord) {
+//        System.out.println("**** Received in REPORT GENERATION GROUP:   ****");
+//        System.out.println(matchRecord.key());
+//        System.out.println(matchRecord.value());
+//        System.out.println(matchRecord.offset());
+//    }
+//
+//    @KafkaListener(topics = {"match-topic"}, groupId = "dashboard-group")
+//    public void dashboardOfMatchStatus(ConsumerRecord<String, String> matchRecord) {
+//        System.out.println("**** Received in dashboard GENERATION GROUP:   ****");
+//        System.out.println(matchRecord.key());
+//        System.out.println(matchRecord.value());
+//        System.out.println(matchRecord.offset());
+//    }
 
 }
