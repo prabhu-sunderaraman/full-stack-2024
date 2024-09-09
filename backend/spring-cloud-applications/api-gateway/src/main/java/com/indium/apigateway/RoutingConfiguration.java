55baed681cfea432e23363cc4839dae6056936c9
Prabhu Sunderaraman <prabhu.bits@gmail.com>
2024-09-09 12:51:12 +0530

    Day29

---
+++ backend/spring-cloud-applications/api-gateway/src/main/java/com/indium/apigateway/RoutingConfiguration.java
@@ -0,0 +1,18 @@
+package com.indium.apigateway;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RoutingConfiguration {
+
+//    @Bean
+//    public Route customRouteLocator(RouteLocatorBuilder builder) {
+//        return builder.routes()
+//                .route("cube", r -> r.path("/get")
+//                        .uri("lb://cube-api"))
+//                .route("square", r -> r.path("/get")
+//                        .uri("lb://square-api"))
+//                .build();
+//    }
+}
