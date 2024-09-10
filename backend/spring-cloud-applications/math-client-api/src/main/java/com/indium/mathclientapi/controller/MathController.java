6d20425878339416ce57e022321bb1637a7e08d3
Prabhu Sunderaraman <prabhu.bits@gmail.com>
2024-09-10 11:07:34 +0530

    Day30

--- backend/spring-cloud-applications/math-client-api/src/main/java/com/indium/mathclientapi/controller/MathController.java
+++ backend/spring-cloud-applications/math-client-api/src/main/java/com/indium/mathclientapi/controller/MathController.java
@@ -2,6 +2,7 @@ package com.indium.mathclientapi.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.circuitbreaker.CircuitBreakerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,10 +22,28 @@ public class MathController {
     @Value("${cube-api.url}")
     private String cubeApiUrl;
 
+    @Autowired
+    private CircuitBreakerFactory circuitBreakerFactory;
+
     @GetMapping("/cube/{number}")
     public String getCubeOfNumber(@PathVariable int number) {
-        int cube = restTemplate.getForObject(cubeApiUrl + "/" + number, Integer.class);
-        return "Cube of " + number + " is " + cube;
+        return circuitBreakerFactory
+                .create("cube-circuit")
+                .run(() -> {
+                            System.out.println("******* Trying to call cube service for " + number);
+                            try {
+                                int cube = restTemplate.getForObject(cubeApiUrl + "/" + number, Integer.class);
+                                return "Cube of " + number + " is " + cube;
+                            } catch (Exception ex) {
+//                                System.out.println("Error: " + ex.getMessage());
+                                throw ex;
+                            }
+                        },
+                        throwable -> {
+//                     Usually we call another service and email the administrator or raise an alert
+                            System.out.println("======= Fallback of cube service called for " + number);
+                            return "Oops! Service is down! Cube of " + number + " is not available at the moment";
+                        });
     }
 
     @GetMapping("/square/{number}")
