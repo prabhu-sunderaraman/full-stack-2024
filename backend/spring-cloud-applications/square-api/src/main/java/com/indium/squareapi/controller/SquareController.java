56648d08794291afcce312425d6317596abcf78d
Prabhu Sunderaraman <prabhu.bits@gmail.com>
2024-09-11 10:47:21 +0530

    Day31

--- backend/spring-cloud-applications/square-api/src/main/java/com/indium/squareapi/controller/SquareController.java
+++ backend/spring-cloud-applications/square-api/src/main/java/com/indium/squareapi/controller/SquareController.java
@@ -4,6 +4,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,15 @@ import org.springframework.web.bind.annotation.RestController;
 @Tag(name = "Square of a number", description = "Endpoints to get the square of a number")
 @RestController
 public class SquareController {
+
+    @Value("${welcome.message}")
+    private String welcomeMessage;
+
+    @GetMapping("/welcome")
+    public String getWelcomeMessage() {
+        return welcomeMessage;
+    }
+
 
     @Operation(summary = "Get the square of a number", parameters = {
             @Parameter(name = "number", description = "number", example = "123")
