6d3b6884f38e8948342b757e37ee947a8f7fdc84
Prabhu Sunderaraman <prabhu.bits@gmail.com>
2024-09-13 12:48:18 +0530

    Day33

--- backend/ut-project/testing-spring-boot-api/src/main/java/com/indium/testingspringbootapi/controller/WelcomeController.java
+++ backend/ut-project/testing-spring-boot-api/src/main/java/com/indium/testingspringbootapi/controller/WelcomeController.java
@@ -1,6 +1,7 @@
 package com.indium.testingspringbootapi.controller;
 
 import com.indium.testingspringbootapi.service.TrainingService;
+import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -10,21 +11,33 @@ public class WelcomeController {
     @Autowired
     private TrainingService trainingService;
 
+    private static Logger logger = org.slf4j.LoggerFactory.getLogger(WelcomeController.class);
+
     @GetMapping("/welcome")
     public String welcome() {
+        System.out.println("****welcome called****");
+        logger.info("****welcome called****");
         return "Hello, World!";
     }
 
     @GetMapping("/hello/{name}")
     public String hello(@PathVariable String name) {
+        if (name.equalsIgnoreCase("error")) {
+            logger.warn("****useless name provided {}****", name);
+        }
         return "Hello, " + name + "!";
     }
 
     @PostMapping("/register")
     public String registerForTraining(@RequestParam String email) {
-        boolean registered = trainingService.register(email);
-        if (!registered) {
-            return "Failed to register for training with email: " + email;
+        try {
+            boolean registered = trainingService.register(email);
+            if (!registered) {
+                return "Failed to register for training with email: " + email;
+            }
+            logger.info("****Registered for training with email: {}****", email);
+        } catch (Exception e) {
+            logger.error("****Failed to register for training with email: {}, error is {}****", email, e.getMessage());
         }
         return "Registered for training with email: " + email;
     }
--- backend/ut-project/testing-spring-boot-api/src/main/java/com/indium/testingspringbootapi/service/TrainingService.java
+++ backend/ut-project/testing-spring-boot-api/src/main/java/com/indium/testingspringbootapi/service/TrainingService.java
@@ -1,10 +1,15 @@
 package com.indium.testingspringbootapi.service;
 
+import org.slf4j.Logger;
 import org.springframework.stereotype.Service;
 
 @Service
 public class TrainingService {
+
+    private static Logger logger = org.slf4j.LoggerFactory.getLogger(TrainingService.class);
+
     public boolean register(String email) {
+        logger.info("****registering email {}****", email);
         // call repository to save email or an external service
         return true;
     }
