ef8c0bb6f48844d8121990304101748ebaed5e02
Prabhu Sunderaraman <prabhu.bits@gmail.com>
2024-09-19 09:55:53 +0530

    Day35

---
+++ backend/misc/java-app/src/main/java/com/indium/DesignPatterns.java
@@ -0,0 +1,22 @@
+package com.indium;
+
+public class DesignPatterns {
+    public static void main(String[] args) {
+        //Commonly occurring problems and their solutions
+        //Every pattern has a name, problem and solution
+        //Gang of Four (GoF) patterns
+        //four authors - Erich Gamma, Richard Helm, Ralph Johnson, John Vlissides
+        //Creational, Structural, Behavioral
+
+        //Creational - Singleton, Factory, Abstract Factory, Builder, Prototype
+        //Creational patterns deal with the way objects are created
+
+        //Structural - Adapter, Bridge, Composite, Decorator, Facade, Flyweight, Proxy
+        //Structural patterns deal with the composition of classes or objects
+
+        //Behavioral - Chain of Responsibility, Command, Interpreter, Iterator, Mediator, Memento, Observer, State, Strategy, Template Method, Visitor
+        //Behavioral patterns deal with the communication between objects
+
+
+    }
+}
---
+++ backend/misc/java-app/src/main/java/com/indium/SingletonPattern.java
@@ -0,0 +1,40 @@
+package com.indium;
+
+public class SingletonPattern {
+    public static void main(String[] args) {
+        //Singleton Pattern - Creational Pattern
+        //Ensures that a class has only one instance and provides a global point of access to it
+
+//        VendingMachine princyVendingMachine = new VendingMachine();
+//        VendingMachine lakshmiVendingMachine = new VendingMachine();
+//        VendingMachine tarunVendingMachine = new VendingMachine();
+
+        VendingMachine princyVendingMachine = VendingMachine.getVendingMachine();
+        princyVendingMachine.drinkCoffee();
+        VendingMachine lakshmiVendingMachine = VendingMachine.getVendingMachine();
+        lakshmiVendingMachine.drinkCoffee();
+        VendingMachine tarunVendingMachine = VendingMachine.getVendingMachine();
+        tarunVendingMachine.drinkCoffee();
+
+    }
+}
+
+//Singleton class
+class VendingMachine {
+    private static VendingMachine vendingMachine = new VendingMachine();
+
+    private VendingMachine() {
+    }
+
+    public static VendingMachine getVendingMachine() {
+        return vendingMachine;
+    }
+
+    public void refill() {
+        System.out.println("Vending machine is refilled");
+    }
+
+    public void drinkCoffee() {
+        System.out.println("Coffee is served");
+    }
+}
